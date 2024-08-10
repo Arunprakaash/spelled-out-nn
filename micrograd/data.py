a53a27cdaed5d04bf6bd1bc2bbb55bfe6724a3bb
Arunprakaash <prakaasharun50@gmail.com>
2024-08-10 11:22:44 -0700

    forward pass

--- micrograd/data.py
+++ micrograd/data.py
@@ -1,34 +1,46 @@
+import math
 from typing import Text
 
-class Data:  
-  def __init__(self, value, _children=(), _operator: Text = '', label: Text = ''):
-    self.value = value
-    self.grad = 0.0
-    self._prev = set(_children)
-    self._operator = _operator
-    self.label = label
-
-  def __repr__(self):
-    return f"Data(value={self.value})"
-  
-  def __add__(self, other):
-    # other = other if isinstance(other, Value) else Value(other)
-    result = Data(value = self.value + other.value, _children = (self, other), _operator = '+')
+class Data:
+    def __init__(self, value, _children=(), _operator: Text = '', label: Text = ''):
+        self.value = value
+        self.grad = 0.0
+        self._prev = set(_children)
+        self._operator = _operator
+        self.label = label
     
-    # def _backward():
-    #   self.grad += 1.0 * out.grad
-    #   other.grad += 1.0 * out.grad
-    # out._backward = _backward
-    
-    return result
-
-  def __mul__(self, other):
-    # other = other if isinstance(other, Value) else Value(other)
-    result = Data(value = self.value * other.value, _children = (self, other), _operator = '*')
-    
-    # def _backward():
-    #   self.grad += other.data * out.grad
-    #   other.grad += self.data * out.grad
-    # out._backward = _backward
+    def __repr__(self):
+        return f"Data(value={self.value})"
       
-    return result
+    def __add__(self, other):
+        # other = other if isinstance(other, Value) else Value(other)
+        result = Data(value = self.value + other.value, _children = (self, other), _operator = '+')
+        
+        # def _backward():
+        #   self.grad += 1.0 * out.grad
+        #   other.grad += 1.0 * out.grad
+        # out._backward = _backward
+        
+        return result
+    
+    def __mul__(self, other):
+        # other = other if isinstance(other, Value) else Value(other)
+        result = Data(value = self.value * other.value, _children = (self, other), _operator = '*')
+        
+        # def _backward():
+        #   self.grad += other.data * out.grad
+        #   other.grad += self.data * out.grad
+        # out._backward = _backward
+          
+        return result
+
+    def tanh(self):
+        x = self.value
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Data(t, (self, ), 'tanh')
+    
+        # def _backward():
+        # self.grad += (1 - t**2) * out.grad
+        # out._backward = _backward
+    
+        return out
