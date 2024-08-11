ac21a1292a1bcac0afe87b415f997b7b8aec1c17
Arunprakaash <prakaasharun50@gmail.com>
2024-08-10 20:31:21 -0700

    backpropagation method in Data class

--- micrograd/data.py
+++ micrograd/data.py
@@ -5,6 +5,7 @@ class Data:
     def __init__(self, value, _children=(), _operator: Text = '', label: Text = ''):
         self.value = value
         self.grad = 0.0
+        self._backward = lambda: None
         self._prev = set(_children)
         self._operator = _operator
         self.label = label
@@ -13,34 +14,51 @@ class Data:
         return f"Data(value={self.value})"
       
     def __add__(self, other):
-        # other = other if isinstance(other, Value) else Value(other)
+        other = other if isinstance(other, Data) else Data(other)
         result = Data(value = self.value + other.value, _children = (self, other), _operator = '+')
         
-        # def _backward():
-        #   self.grad += 1.0 * out.grad
-        #   other.grad += 1.0 * out.grad
-        # out._backward = _backward
+        def _backward():
+          self.grad += 1.0 * result.grad
+          other.grad += 1.0 * result.grad
+        result._backward = _backward
         
         return result
     
     def __mul__(self, other):
-        # other = other if isinstance(other, Value) else Value(other)
+        other = other if isinstance(other, Data) else Data(other)
         result = Data(value = self.value * other.value, _children = (self, other), _operator = '*')
         
-        # def _backward():
-        #   self.grad += other.data * out.grad
-        #   other.grad += self.data * out.grad
-        # out._backward = _backward
+        def _backward():
+          self.grad += other.value * result.grad
+          other.grad += self.value * result.grad
+        result._backward = _backward
           
         return result
 
     def tanh(self):
         x = self.value
         t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
-        out = Data(t, (self, ), 'tanh')
+        result = Data(t, (self, ), 'tanh')
     
-        # def _backward():
-        # self.grad += (1 - t**2) * out.grad
-        # out._backward = _backward
+        def _backward():
+            self.grad += (1 - t**2) * result.grad
+        result._backward = _backward
     
-        return out
+        return result
+
+    def backward(self):
+        topo = []
+        visited = set()
+        
+        def build_topo(v):
+          if v not in visited:
+            visited.add(v)
+            for child in v._prev:
+              build_topo(child)
+            topo.append(v)
+        
+        build_topo(self)
+        
+        self.grad = 1.0
+        for node in reversed(topo):
+          node._backward()
