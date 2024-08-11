8263a3493b4e7ea1e38c7f8bedd1e819c91900d3
Arunprakaash <prakaasharun50@gmail.com>
2024-08-10 23:17:12 -0700

    backpropagation in mlp setting

--- micrograd/data.py
+++ micrograd/data.py
@@ -35,6 +35,32 @@ class Data:
           
         return result
 
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        result = Data(self.value**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.value ** (other - 1)) * result.grad
+
+        result._backward = _backward
+    
+        return result
+  
+    def __rmul__(self, other):
+        return self * other
+    
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __radd__(self, other):
+        return self + other
+
     def tanh(self):
         x = self.value
         t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
@@ -44,6 +70,17 @@ class Data:
             self.grad += (1 - t**2) * result.grad
         result._backward = _backward
     
+        return result
+
+    def exp(self):
+        x = self.value
+        result = Data(math.exp(x), (self, ), 'exp')
+        
+        def _backward():
+          self.grad += result.value * result.grad
+
+        result._backward = _backward
+        
         return result
 
     def backward(self):
